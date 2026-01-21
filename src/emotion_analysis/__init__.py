d00f5d8caca8e0248ba26ffe089d098101c81c20
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2026-01-21 22:23:38 +0000

    feat: create dunder init and main files

---
+++ src/emotion_analysis/__init__.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+from .app import build_gui, on_classify
+from .services import Classifier
+from .settings import Settings
+from .utils import setup_envvars, setup_log
+
+
+def main(settings: Settings):
+    setup_envvars(settings.GRADIO.env, settings.HF.env)
+    setup_log(settings.LOG)
+
+    model = Classifier(settings.MODELS)
+    pprint(model.id2label)
+    pprint(model.label2id)
+
+    build_gui(model, on_btn_classify=on_classify).launch(settings.GRADIO.config)
+
+
+if __name__ == "__main__":
+    main(Settings())
---
+++ src/emotion_analysis/__main__.py
@@ -0,0 +1,3 @@
+from . import Settings, main
+
+main(Settings())
