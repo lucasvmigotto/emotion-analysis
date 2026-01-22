ade3cb647c7f4f0f4a93bd81c25a284a3197edcd
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2026-01-22 14:37:07 +0000

    fix(settings): update settings submodules
    Add token to HF
    Change default log level to INFO
    Change model id in models and set feature extractor to None

--- src/emotion_analysis/settings/hf.py
+++ src/emotion_analysis/settings/hf.py
@@ -6,7 +6,12 @@ from pydantic import BaseModel, computed_field
 class HuggingFaceSettings(BaseModel):
     HOME: str = "/hf"
 
+    TOKEN: str | None = None
+
     @computed_field
     @property
     def env(self: Self) -> dict[str, str]:
-        return {"HF_HOME": self.HOME}
+        return {
+            "HF_HOME": self.HOME,
+            "HF_TOKEN": self.TOKEN
+        }
--- src/emotion_analysis/settings/models.py
+++ src/emotion_analysis/settings/models.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class ModelsSettings(BaseModel):
-    MODEL_ID: str = "lucasvmigotto/whisper-small-audio-emotion-classification"
+    MODEL_ID: str = "firdhokk/speech-emotion-recognition-with-openai-whisper-large-v3"
 
-    FEATURE_EXTRACTOR_ID: str = "openai/whisper-small"
+    FEATURE_EXTRACTOR_ID: str | None = None
